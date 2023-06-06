43c703fb916761c07be54dd7f086541e92e464bd
K-Randall <kayla.randall@willowtreeapps.com>
2023-06-06 10:47:47 -0400

    Initializing todo application
    - edited App.tsx file
    - created Task.tsx and List.tsx

--- Chapter-3/todo/src/App.tsx
+++ Chapter-3/todo/src/App.tsx
@@ -1,26 +1,48 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+//defining some initial tasks and creating the todoList state,
+//which we will pass to the list component
+
+import {useState, useEffect, useMemo, useCallback} from 'react'
+
+import List, {Todo} from './List'
+
+const initialTodos = [
+  {id: 1, task: 'Go grocery shopping'},
+  {id: 2, task: 'Pay rent'}
+]
 
 function App() {
+  const [todoList, setTodoList] = useState(initialTodos)
+  const [task, setTask] = useState('')
+
+  useEffect(() => {
+    console.log('Rendering <App />'
+    )
+  })
+
+  const handleCreate = () => {
+    const newTodo = {
+      id: Date.now(),
+      task
+    }
+    //Pushing the new todo to the list 
+    setTodoList([...todoList, newTodo])
+    //Resetting input value
+    setTask('')
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <>
+      <input
+        type='text'
+        value={task}
+        onChange={(e) => setTask(e.target.value)}
+      />
+
+      <button onClick={handleCreate}>Create</button>
+
+      <List todoList={todoList} />
+    </>
+  )
 }
 
-export default App;
+export default App
---
+++ Chapter-3/todo/src/List.tsx
@@ -0,0 +1,33 @@
+//We are rendering each task of the todoList array by using the TAsk component
+// and we pask task as a prop.
+// The useEffect is to see how many renders the app is performing
+
+import {FC, useEffect} from 'react'
+import Task from './Task'
+
+//Types
+export type Todo = {
+    id: number,
+    task: string
+}
+
+interface Props {
+    todoList: Todo[]
+}
+
+const List: FC<Props> = ({ todoList }) => {
+    useEffect(() => {
+        //This effect is executed every new render
+        console.log('Rendering <List/>')
+    })
+
+    return (
+        <ul>
+            {todoList.map((todo: Todo) => (
+                <Task key={todo.id} id={todo.id} task={todo.task} />
+            ))}
+        </ul>
+    )
+}
+
+export default List
